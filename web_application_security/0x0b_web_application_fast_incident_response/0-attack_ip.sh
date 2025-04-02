0fff0e16cd860014b56a9b853586dd75986cca1a
fitowashere <fito_washere@icloud.com>
2025-04-01 21:27:18 -0400

    Update IP extraction script to use 'logs.txt' as input file

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Check if the log file exists
-if [ ! -f $1 ]
-then
-    echo "Error: Log file not found."
-    exit 1
-fi
-
-# Extract IP addresses from the log file, count occurrences, and find the most frequent one
-grep -o -E '([0-9]{1,3}\.){3}[0-9]{1,3}' $1 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+# Extract IP addresses from logs.txt, count occurrences, and find the most frequent one
+grep -o -E '([0-9]{1,3}\.){3}[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
