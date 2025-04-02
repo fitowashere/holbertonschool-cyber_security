25e0ee9795c8595cee537d7e3095333357f6e725
fitowashere <fito_washere@icloud.com>
2025-04-01 21:38:42 -0400

    Refactor IP and User-Agent extraction logic for improved efficiency and accuracy

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# First find attacker IP
-attacker_ip=$(grep -o -E '([0-9]{1,3}\.){3}[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -1 | awk '{print $2}')
-
-# Extract the most common User-Agent used by this IP
-grep "$attacker_ip" logs.txt | grep -o "User-Agent:.*" | head -1 | sed 's/.*User-Agent: //' | sed 's/"$//'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}' | xargs -I{} awk -v ip="{}" '$1 == ip {print $0}' logs.txt | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2, $3}'
