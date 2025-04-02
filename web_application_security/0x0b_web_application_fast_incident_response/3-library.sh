d71d5362e762c9d115c73829a569b99b75c4cdc1
fitowashere <fito_washere@icloud.com>
2025-04-01 21:34:23 -0400

    Refactor User-Agent extraction in IP extraction script for improved readability

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -4,4 +4,4 @@
 attacker_ip=$(grep -o -E '([0-9]{1,3}\.){3}[0-9]{1,3}' logs.txt | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
 
 # Filter logs for the attacker IP and extract User-Agent
-grep "$attacker_ip" logs.txt | grep -o "User-Agent: [^\"]*" | cut -d' ' -f2- | sort | uniq -c | sort -nr | head -n 1 | awk '{for(i=2;i<=NF;i++) printf "%s%s", $i, (i<NF ? " " : "\n")}'
+grep "$attacker_ip" logs.txt | grep -o "User-Agent: [^\"]*" | sed 's/User-Agent: //' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ //'
