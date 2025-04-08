109d6a2aefc54590b440b243b7e6fe072cc67943
fitowashere <fito_washere@icloud.com>
2025-04-08 19:05:28 -0400

    Refactor firewall script to simplify rule addition pattern matching for iptables

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Look for various patterns that might indicate firewall rule additions
-cat auth.log | grep -E '(firewall|iptables|ufw|DROP|ACCEPT|REJECT|filter|chain)' | grep -iE '(add|new|creat|insert|append)' | grep -v 'invalid\|failed\|failure\|error\|denied' | wc -l
+grep "iptables" auth.log | grep "INPUT" | wc -l
