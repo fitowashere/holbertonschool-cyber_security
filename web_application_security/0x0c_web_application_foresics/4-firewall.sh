ebcce4adaedd9045f1681c7ecb3b77c259585f91
fitowashere <fito_washere@icloud.com>
2025-04-08 19:04:51 -0400

    Enhance firewall script to filter out invalid rule additions and broaden pattern matching

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep -E "firewall|iptables|ufw|pf|netfilter|RULE" auth.log | grep -iE "add|append|new|create" | wc -l
+# Look for various patterns that might indicate firewall rule additions
+cat auth.log | grep -E '(firewall|iptables|ufw|DROP|ACCEPT|REJECT|filter|chain)' | grep -iE '(add|new|creat|insert|append)' | grep -v 'invalid\|failed\|failure\|error\|denied' | wc -l
