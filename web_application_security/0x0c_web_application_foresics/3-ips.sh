8fbd5841c6113cf186761d1835de944a410894e3
fitowashere <fito_washere@icloud.com>
2025-04-08 19:02:55 -0400

    Update script to count unique successful login attempts for root from auth.log

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Count unique IP addresses that successfully gained access
-grep "Accepted" auth.log | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort -u | wc -l
+grep "Accepted password for root" auth.log | awk '{print$(NF-3)}' | sort | uniq -c | wc -l
