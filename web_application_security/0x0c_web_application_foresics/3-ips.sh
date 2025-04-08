c76b668eda68173b40ea3a22ad1da31a5ba8d07b
fitowashere <fito_washere@icloud.com>
2025-04-08 19:01:59 -0400

    Update script to count unique IP addresses that successfully gained access from auth.log

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep "Accepted password" auth.log | awk '{print $(NF-3)}' | sort -u | wc -l
+# Count unique IP addresses that successfully gained access
+grep "Accepted" auth.log | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | sort -u | wc -l
