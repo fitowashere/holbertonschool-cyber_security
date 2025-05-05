fd9f7f4cecdaf434207ecae29e52988a7d28da84
fitowashere <fito_washere@icloud.com>
2025-05-05 19:34:31 -0400

    Add confirmation message after merging JSON files in write_file.rb

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -16,4 +16,7 @@ def merge_json_files(file1_path, file2_path)
 
   # Write the merged data back to file2
   File.write(file2_path, JSON.pretty_generate(merged_data))
+
+  # Print the expected message
+  puts "Merged JSON written to #{file2_path}"
 end
