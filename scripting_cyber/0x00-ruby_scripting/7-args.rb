22d7c37b075efa4df7f2fbedf36bc35c2314281a
fitowashere <fito_washere@icloud.com>
2025-05-05 19:39:26 -0400

    Enhance argument printing function to improve output clarity

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,13 +2,13 @@
 
 # Function that prints each command-line argument
 def print_arguments
-    # Check if any arguments were provided
     if ARGV.empty?
       puts "No arguments provided."
     else
-      # Print each argument with its index
+      puts "Arguments:"
       ARGV.each_with_index do |arg, index|
         puts "#{index + 1}. #{arg}"
       end
     end
-end
+  end
+
