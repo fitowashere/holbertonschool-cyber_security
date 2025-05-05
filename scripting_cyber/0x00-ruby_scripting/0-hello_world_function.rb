d6625271489a8d08a94cc69dd09bcad1aab462e9
fitowashere <fito_washere@icloud.com>
2025-05-05 18:58:40 -0400

    Add hello_world_function.rb to greet with a customizable message

---
+++ scripting_cyber/0x00-ruby_scripting/0-hello_world_function.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Define a function that accepts one string argument
+# and prints "Hello, Holberton! from <str>"
+def say_hello(str)
+    puts "Hello, Holberton! from #{str}"
+end
